d02c954c19c6143ae695f1a05be7128950e5e22b
NghiNguyen <nghinguyenzero@gmail.com>
2024-03-27 01:43:34 +0700

    form product, add product success

--- src/app/admin-view/add-product/page.js
+++ src/app/admin-view/add-product/page.js
@@ -1,95 +1,156 @@
 "use client";
+import { addNewProduct } from "@/app/services/product";
 import InputComponent from "@/components/FormElements/InputComponent";
 import SelectComponent from "@/components/FormElements/SelectComponent";
 import TileComponent from "@/components/TileComponent";
 import {
-  AvailableSizes,
-  adminAddProductformControls,
-  firebaseConfig,
-  firebaseStorageURL,
+    AvailableSizes,
+    adminAddProductformControls,
+    firebaseConfig,
+    firebaseStorageURL,
 } from "@/utils";
 import { initializeApp } from "firebase/app";
 import {
-  getDownloadURL,
-  getStorage,
-  ref,
-  uploadBytesResumable,
+    getDownloadURL,
+    getStorage,
+    ref,
+    uploadBytesResumable,
 } from "firebase/storage";
+import { useState } from "react";
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const storage = getStorage(app, firebaseStorageURL);
 
 const createUniqueFileName = (getFile) => {
-  const timestamp = Date.now();
-  const randomStringValue = Math.random().toString(36).substring(2, 12);
-  return `${getFile.name} -${timestamp}-${randomStringValue}`;
+    const timestamp = Date.now();
+    const randomStringValue = Math.random().toString(36).substring(2, 12);
+    return `${getFile.name} -${timestamp}-${randomStringValue}`;
 };
 
 async function helperForUploadingImageToFirebase(file) {
-  const getFileName = createUniqueFileName(file);
-  const storageReference = ref(storage, `ecommerce/${getFileName}`);
-  const uploadImage = uploadBytesResumable(storageReference, file);
-  return new Promise((resolve, reject) => {
-    uploadImage.on(
-      "state_changed",
-      (snapshot) => {},
-      (error) => {
-        console.log(error);
-        reject(error);
-      },
-      () => {
-        getDownloadURL(uploadImage.snapshot.ref)
-          .then((dowloadUrl) => resolve(dowloadUrl))
-          .catch((error) => reject(error));
-      }
-    );
-  });
+    const getFileName = createUniqueFileName(file);
+    const storageReference = ref(storage, `ecommerce/${getFileName}`);
+    const uploadImage = uploadBytesResumable(storageReference, file);
+    return new Promise((resolve, reject) => {
+        uploadImage.on(
+            "state_changed",
+            (snapshot) => { },
+            (error) => {
+                console.log(error);
+                reject(error);
+            },
+            () => {
+                getDownloadURL(uploadImage.snapshot.ref)
+                    .then((dowloadUrl) => resolve(dowloadUrl))
+                    .catch((error) => reject(error));
+            }
+        );
+    });
+}
+
+const initFormData = {
+    name: "",
+    price: 0,
+    description: "",
+    category: 'men',
+    sizes: [],
+    deliveryInfo: '',
+    onSale: 'no',
+    imageUrl: '',
+    priceDrop: 0
 }
 
 export default function AdminAddNewProduct() {
-  async function handleImage(event) {
-    console.log(event.target.files);
-    const extractImageUrl = await helperForUploadingImageToFirebase(
-      event.target.files[0]
-    );
-    console.log(extractImageUrl);
-  }
+    const [formData, setFormData] = useState(initFormData);
 
-  return (
-    <div className="w-full mt-5 mr-0 mb-0 ml-0 relative">
-      <div className="flex flex-col items-start justify-start p-10 bg-white shadow-2xl rounded-xl relative">
-        <div className="w-full mt-6 mr-0 mb-0 ml-0 space-y-8">
-          <input
-            accept="image/*"
-            max="1000000"
-            type="file"
-            onChange={handleImage}
-          />
+    async function handleImage(event) {
+        const extractImageUrl = await helperForUploadingImageToFirebase(
+            event.target.files[0]
+        )
+        if(extractImageUrl) {
+            setFormData({
+                ...formData,
+                imageUrl: extractImageUrl
+            })
+        }
+    }
+    console.log({formData});
 
-          <div className="flex gap-2 flex-col">
-            <label>Available sizes</label>
-            <TileComponent data={AvailableSizes} />
-          </div>
-          {adminAddProductformControls.map((controlItem) =>
-            controlItem.componentType === "input" ? (
-              <InputComponent
-                type={controlItem.type}
-                placeholder={controlItem.placeholder}
-                label={controlItem.label}
-              />
-            ) : controlItem.componentType === "select" ? (
-              <SelectComponent
-                label={controlItem.label}
-                options={controlItem.options}
-              />
-            ) : null
-          )}
-          <button className="inline-flex w-full items-center justify-center bg-black px-6 py-4 text-lg text-white font-medium uppercase tracking-wide">
-            Add Product
-          </button>
+    function handleTileClick(getCurrentItem) {
+        console.log({getCurrentItem});
+        let cpySizes = [...formData.sizes]
+        const index = cpySizes.findIndex(item => item.id === getCurrentItem.id)
+        if(index===-1) {
+            cpySizes.push(getCurrentItem)
+        } else {
+            cpySizes = cpySizes.filter(item=> item.id !== getCurrentItem.id)
+        }
+        setFormData({
+            ...formData,
+            sizes: cpySizes
+        })
+    }
+
+    async function handleAddProduct () {
+        const res = await addNewProduct(formData)
+        console.log({res});
+
+    } 
+
+    return (
+        <div className="w-full mt-5 mr-0 mb-0 ml-0 relative">
+            <div className="flex flex-col items-start justify-start p-10 bg-white shadow-2xl rounded-xl relative">
+                <div className="w-full mt-6 mr-0 mb-0 ml-0 space-y-8">
+                    <input
+                        accept="image/*"
+                        max="1000000"
+                        type="file"
+                        onChange={handleImage}
+                    />
+
+                    <div className="flex gap-2 flex-col">
+                        <label>Available sizes</label>
+                        <TileComponent 
+                            selected={formData.sizes}
+                            data={AvailableSizes} 
+                            onClick={handleTileClick}
+                        />
+                    </div>
+                    {adminAddProductformControls.map((controlItem) =>
+                        controlItem.componentType === "input" ? (
+                            <InputComponent
+                                type={controlItem.type}
+                                placeholder={controlItem.placeholder}
+                                label={controlItem.label}
+                                value={formData[controlItem.id]}
+                                onChange={(event)=>{
+                                    setFormData({
+                                        ...formData,
+                                        [controlItem.id]: event.target.value
+                                    })
+                                }}
+                            />
+                        ) : controlItem.componentType === "select" ? (
+                            <SelectComponent
+                                label={controlItem.label}
+                                options={controlItem.options}
+                                onChange={(event)=>{
+                                    setFormData({
+                                        ...formData,
+                                        [controlItem.id]: event.target.value
+                                    })
+                                }}
+                            />
+                        ) : null
+                    )}
+                    <button 
+                        onClick={handleAddProduct}
+                        className="inline-flex w-full items-center justify-center bg-black px-6 py-4 text-lg text-white font-medium uppercase tracking-wide">
+                        Add Product
+                    </button>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
